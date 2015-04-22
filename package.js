8cf3b79187a403573adea762df8523d0ccf89132
Benjamin Fox <foxbenjaminfox@gmail.com>
2015-04-22 12:34:07 +0300

    Define package.js.

---
+++ package.js
@@ -0,0 +1,30 @@
+Package.describe({
+  summary: "Javascript dialect with fewer braces and semicolons",
+  version: "0.0.0"
+});
+
+Package.registerBuildPlugin({
+  name: "compileCoffeescript",
+  use: [],
+  sources: [
+    'plugin/compile-coffeescript.js'
+  ],
+  npmDependencies: {"coffee-script": "1.7.1", "source-map": "0.1.32"}
+});
+
+Package.onTest(function (api) {
+  api.use(['coffee:script', 'tinytest']);
+  api.use(['coffeescript-test-helper'], ['client', 'server']);
+  api.addFiles('bare_test_setup.coffee', ['client'], {bare: true});
+  api.addFiles('bare_tests.js', ['client']);
+  api.addFiles([
+    'coffeescript_test_setup.js',
+    'tests/coffeescript_tests.coffee',
+    'tests/coffeescript_strict_tests.coffee',
+    'tests/litcoffeescript_tests.litcoffee',
+    'tests/litcoffeescript_tests.coffee.md',
+    'tests/testTemplate.helpers.coffee',
+    'tests/testTemplate.events.coffee',
+    'coffeescript_tests.js'
+  ], ['client', 'server']);
+});
