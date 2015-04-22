f36d7dddd7d07d1611afda380f3ae43c1bef8971
Benjamin Fox <foxbenjaminfox@gmail.com>
2015-04-22 15:44:32 +0300

    Bump version number.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Javascript dialect with fewer braces and semicolons",
-  version: "1.0.1",
+  version: "1.0.2",
   git: "https://github.com/foxbenjaminfox/meteor-coffeescript.git"
 });
 
